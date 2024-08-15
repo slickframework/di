70664df7286f716bfc9dd70aabc715ea912f955d
Filipe Silva <silvam.filipe@gmail.com>
2024-08-15 14:13:52 +0000

    Removed obsolete files and documentation

--- src/Definition/AbstractDefinition.php
+++ src/Definition/AbstractDefinition.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of slick/di
  *
- * For the full copyright and license information, please view the LICENSE
+ * For the full copyright and license information, please view the LICENSE.md
  * file that was distributed with this source code.
  */
 
